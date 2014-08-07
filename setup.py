f2c37a505bc2d810c3e8cc564a4ff87656c25c98
dantezhu <dantezhu@qq.com>
2014-08-07 22:03:54 +0800

    优化一下setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="flylog",
     version='0.1.19',
     zip_safe=False,
     platforms='any',
-    packages=['flylog'],
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     scripts=['flylog/bin/run_flylog_agent.py'],
     url="https://github.com/dantezhu/flylog",
     license="BSD",
