216882b5344f3cf9abd526785bf84511cabece9a
dantezhu <dantezhu@qq.com>
2016-11-02 15:09:42 +0800

    udp server 继承后修改参数

--- flylog/agent/agent.py
+++ flylog/agent/agent.py
@@ -101,7 +101,11 @@ class Agent(object):
                 except:
                     logger.error('exc occur.', exc_info=True)
 
-        server = SocketServer.ThreadingUDPServer((host, port), ThreadedUDPRequestHandler)
+        class MyUDPServer(SocketServer.ThreadingUDPServer):
+            daemon_threads = True
+            allow_reuse_address = True
+
+        server = MyUDPServer((host, port), ThreadedUDPRequestHandler)
 
         try:
             server.serve_forever()
