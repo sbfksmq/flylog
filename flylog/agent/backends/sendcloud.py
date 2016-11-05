afcf28f3d1581b7ff3f57b5d7ac5c6d6a9db3c93
dantezhu <dantezhu@qq.com>
2016-11-05 21:00:24 +0800

    貌似sendcloud可以

---
+++ flylog/agent/backends/sendcloud.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+from ..log import logger
+
+
+class SendCloudBackend(object):
+    """
+    sendcloud发送邮件
+    """
+
+    SEND_URL = 'http://api.sendcloud.net/apiv2/mail/send'
+
+    def __init__(self, api_user, api_key, sender):
+        """
+        初始化
+        """
+        self.api_user = api_user
+        self.api_key = api_key
+        self.sender = sender
+
+    def emit(self, title, content, receiver_list):
+        """
+        发送
+        """
+
+        params = {
+            'apiUser': self.api_user,
+            'apiKey': self.api_key,
+            'from': self.sender,
+            'to': ';'.join(receiver_list),
+            'subject': title,
+            'html': content,
+        }
+
+        try:
+            rsp = requests.post(self.SEND_URL, params, verify=False)
+
+            if not rsp.json()['result']:
+                logger.error('send fail. status: %s, text: %s, params: %s', rsp.status_code, rsp.text, params)
+                return False
+            else:
+                return True
+
+        except:
+            logger.error('exc occur. params: %s', params, exc_info=True)
+            return False
