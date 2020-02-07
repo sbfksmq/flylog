0712e976f9e922761379375980fb8c57850dd8f0
lijifeng <lijifeng@qfun.com>
2020-02-07 12:54:07 +0800

    添加日志

--- flylog/server/backends/robot.py
+++ flylog/server/backends/robot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import requests
+from ..log import logger
 
 
 class DingRobot(object):
@@ -7,13 +8,15 @@ class DingRobot(object):
     def __init__(self, web_hook_list):
         self.web_hook_list = web_hook_list
 
-    def emit(self, title, content):
+    def emit(self, title, content, user_list=None):
         """
         发送
         """
         full_content = '\n\n'.join([title, content])
         headers = {'Content-Type': 'application/json'}
         data = str({"msgtype": "text", "text": {"content": full_content}})
+        logger.error('trace ding robot data: %s, headers: %s, web_hook: %s', data, headers, self.web_hook_list)
         for web_hook in self.web_hook_list:
-            requests.post(web_hook, data=data, headers=headers)
+            ret = requests.post(web_hook, data=data, headers=headers)
+            logger.error('trace ding robot ret: %s', ret)
         return True
