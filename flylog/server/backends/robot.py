c08d73e3a2dfd376f14af75b9abce354a1297b15
lijifeng <lijifeng@qfun.com>
2020-02-07 14:47:26 +0800

    整理代码

--- flylog/server/backends/robot.py
+++ flylog/server/backends/robot.py
@@ -8,13 +8,13 @@ class DingRobot(object):
     def __init__(self, web_hook_list):
         self.web_hook_list = web_hook_list
 
-    def emit(self, title, content, user_list=None):
-        """
-        发送
-        """
+    def emit(self, title, content):
+
         full_content = '\n\n'.join([title, content])
         headers = {'Content-Type': 'application/json'}
         data = json.dumps({"msgtype": "text", "text": {"content": full_content}})
+
         for web_hook in self.web_hook_list:
-            ret = requests.post(web_hook, data=data, headers=headers)
+            requests.post(web_hook, data=data, headers=headers)
+
         return True
