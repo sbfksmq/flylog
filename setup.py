d9e4e23b477b725406b9e41f57d582ade6f47c06
lijifeng <lijifeng@qfun.com>
2020-02-11 19:28:33 +0800

    版本号修改

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flylog_robot",
-    version='0.0.1',
+    version='0.0.3',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
