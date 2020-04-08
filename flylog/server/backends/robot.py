c637155156a911c31a334936fc772a206c59b177
lijifeng <lijifeng@qfun.com>
2020-04-08 17:57:50 +0800

    remove log

--- flylog/server/backends/robot.py
+++ flylog/server/backends/robot.py
@@ -13,20 +13,13 @@ class DingRobot(object):
 
     def emit(self, title, content, service_name=None):
 
-        logger.info('trace data title: %s, content: %s, service_name: %s, map: %s',
-                    title, content, service_name, self.web_hook_service_map)
-
         full_content = '\n\n'.join([title, content])
         headers = {'Content-Type': 'application/json'}
         data = json.dumps({"msgtype": "text", "text": {"content": full_content}})
 
         res_list = []
-        logger.info('trace data map: %s', self.web_hook_service_map)
         for web_hook, service_list in self.web_hook_service_map.items():
-            logger.info('trace data web_hook1 : %s, service_list: %s', web_hook, service_list)
             if service_name in service_list:
                 rsp = requests.post(web_hook, data=data, headers=headers).json()
-                logger.info('trace data rsp: %s', rsp)
                 res_list.append(rsp['errcode'] in [self.RET_OK, ])
-            logger.info('trace data web_hook2 : %s, service_list: %s', web_hook, service_list)
         return False not in res_list
